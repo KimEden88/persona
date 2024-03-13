5ba4f1d6954ddb8d14085e22f6540836742ad8e8
Kim Eden <kimeden.dev@gmail.com>
2024-03-13 12:52:29 +0100

    Resolving next/font error

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+//fonts
+import { Sora } from "@next/font/google";
+//font setting
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+//components
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={`page bg-site text-white bg-cover bg-no-repeat`}>
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/services/index.js
@@ -0,0 +1,46 @@
+//icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from "react-icons/rx";
+export const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: "Branding",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.",
+  },
+  {
+    icon: <RxPencil2 />,
+    title: "Design",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.",
+  },
+  {
+    icon: <RxDesktop />,
+    title: "Development",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "Copywriting",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy.",
+  },
+];
+
+const Services = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
